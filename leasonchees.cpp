f74115cec08497c3f7ee6cabe41be88ae8dc692c
ojaster <daniil.gusev@icloud.com>
2018-04-14 17:59:09 +0300

    leasonchees.cpp

---
+++ leasonchees.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int x1,x2,y1,y2;
+    cout<<"x1=";
+    cin>>x1;
+    cout<<"x2=";
+    cin>>x2;
+    cout<<"y1=";
+    cin>>y1;
+    cout<<"y2=";
+    cin>>y2;
+    if(x1==x2||y1==y2){
+        cout<<"yes"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
+}
