b7ccf5e1ee1141da6228bef124aa5acf048ea9d7
ojaster <daniil.gusev@icloud.com>
2018-04-15 20:32:56 +0300

    leasoncorova.cpp

---
+++ leasonkorowa.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"n=";
+    cin>>n;
+    switch(n){
+        case 1:
+        cout<<n<<" "<<"corova"<<endl;
+        break;
+         case 2:
+        cout<<n<<" "<<"corovy"<<endl;
+        break;
+         case 5:
+        cout<<n<<" "<<"corov"<<endl;
+        break;
+    }
+}
