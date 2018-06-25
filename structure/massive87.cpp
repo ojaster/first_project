3deb3bf90ccab48b3d54607cc83c55acd3c2ed93
ojaster <daniil.gusev@icloud.com>
2018-06-25 15:19:16 +0300

    sortirovka

--- structure/massive87.cpp
+++ structure/massive87.cpp
@@ -3,15 +3,18 @@ using namespace std;
 int main(){
     const int n=8;
     int k[n];
-    int min=99999999;
     for(int i =0; i<n; i++){
         cin>>k[i];
     }
-    for(int i =0; i<n; i++){
-        if(k[i]<min){
-            min=k[i]; 
+    for(int i =0; i< n - 1; i++){
+        for(int j = 0; j< n - i -1; j++){
+            if(k[j]<k[j+1]){ // 
+                int temp = k[j+1];
+                k[j+1] = k[j];
+                k[j] = temp;
+            }
         }
-    }
+    }   
     cout<<"-----"<<endl;
     for(int i =0; i<n; i++){
         cout<<k[i]<<endl;
