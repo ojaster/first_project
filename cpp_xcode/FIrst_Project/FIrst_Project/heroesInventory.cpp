f57479497fbe8fe8f1f3d0675ceaebd2af63b900
ojaster <daniil.gusev@icloud.com>
2019-01-06 12:04:51 +0200

    heroes Inventory

---
+++ cpp_xcode/FIrst_Project/FIrst_Project/heroesInventory.cpp
@@ -0,0 +1,53 @@
+//
+//  heroesInventory.cpp
+//  FIrst_Project
+//
+//  Created by Данил on 06/01/2019.
+//  Copyright © 2019 Daniil. All rights reserved.
+//
+
+//#include <stdio.h>
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//int main(){
+//    vector<string> inventory;
+//    //inventory[0] = "sword"; так делать нельзя!!!!
+//    //vector<string> inventory1(10, "nothing");
+//    //vector<string> inventory2(inventory1);
+//    
+//    inventory.push_back("sword");//добавляет элемент в конец вектора
+//    inventory.push_back("armor");
+//    inventory.push_back("shield");
+//    
+//    cout<<"You have "<<inventory.size()<<" items."<<endl;
+//    cout<<"Your items:"<<endl;
+//    for(unsigned int i=0; i<inventory.size(); i++){
+//        cout<<inventory[i]<<endl;
+//    }
+//    
+//    cout<<"You trade your sword for a battle axe."<<endl;
+//    inventory[0] = "Battle axe";
+//    
+//    cout<<"Your items:"<<endl;
+//    for(unsigned int i=0; i<inventory.size(); i++){
+//        cout<<inventory[i]<<endl;
+//    }
+//    
+//    cout<<"The item name "<<inventory[0]<<" has "<<inventory[0].size()<<" letters in it"<<endl;
+//    cout<<"Your shield has been destroyed in fierce battle"<<endl;
+//    
+//    inventory.pop_back();//удаляет из вектора последний элемент и уменьшает размер вектора на единицу
+//    cout<<"Your items:"<<endl;
+//    for(unsigned int i=0; i<inventory.size(); i++){
+//        cout<<inventory[i]<<endl;
+//    }
+//    cout<<"You were robbed of all your possessions by a thief"<<endl;
+//    inventory.clear();
+//    if(inventory.empty()){
+//        cout<<"Your have nothing"<<endl;
+//    }else{
+//         cout<<"Your have at least one item"<<endl;
+//    }
+//}
---
+++ cpp_xcode/FIrst_Project/FIrst_Project/heroesInventory2.cpp
@@ -0,0 +1,59 @@
+//
+//  heroesInventory2.cpp
+//  FIrst_Project
+//
+//  Created by Данил on 06/01/2019.
+//  Copyright © 2019 Daniil. All rights reserved.
+//
+
+//#include <stdio.h>
+//#include <iostream>
+//#include <string>
+//#include <vector>
+//using namespace std;
+//int main(){
+//    vector<string> inventory;
+//
+//    inventory.push_back("sword");//добавляет элемент в конец вектора
+//    inventory.push_back("armor");
+//    inventory.push_back("shield");
+//
+//    vector<string>::iterator myIterator;
+//    vector<string>::const_iterator iter;
+//
+//    cout<<"Your items:"<<endl;
+//    for(iter = inventory.begin(); iter != inventory.end(); iter++){
+//        cout<<*iter<<endl; // ->a
+//    }
+//
+//    cout<<"You have "<<inventory.size()<<" items."<<endl;
+//    cout<<"Your items:"<<endl;
+//    for(unsigned int i=0; i<inventory.size(); i++){
+//        cout<<inventory[i]<<endl;
+//    }
+//
+//    cout<<"You trade your sword for a battle axe."<<endl;
+//    myIterator=inventory.begin();
+//    *myIterator = "Battle axe";
+//
+//    cout<<"Your items:"<<endl;
+//    for(iter = inventory.begin(); iter != inventory.end(); iter++){
+//        cout<<*iter<<endl; // ->a
+//    }
+//
+//    cout<<"The item name "<<*myIterator<<" has "<<(*myIterator).size()<<" letters in it"<<endl;
+//    cout<<"Your shield has been destroyed in fierce battle"<<endl;
+//
+//    inventory.pop_back();//удаляет из вектора последний элемент и уменьшает размер вектора на единицу
+//    cout<<"Your items:"<<endl;
+//    for(unsigned int i=0; i<inventory.size(); i++){
+//        cout<<inventory[i]<<endl;
+//    }
+//    cout<<"You were robbed of all your possessions by a thief"<<endl;
+//    inventory.clear();
+//    if(inventory.empty()){
+//        cout<<"Your have nothing"<<endl;
+//    }else{
+//        cout<<"Your have at least one item"<<endl;
+//    }
+//}
